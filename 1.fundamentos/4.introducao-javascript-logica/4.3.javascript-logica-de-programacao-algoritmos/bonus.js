86a040a40b5b2b6617d4f7dfcd00549e1dd67286
saraivais <isadorasaraiva95@gmail.com>
2022-02-05 18:00:13 -0300

    exercicios 4.3

--- 1.fundamentos/4.introducao-javascript-logica/4.3.javascript-logica-de-programacao-algoritmos/bonus.js
+++ 1.fundamentos/4.introducao-javascript-logica/4.3.javascript-logica-de-programacao-algoritmos/bonus.js
@@ -43,3 +43,47 @@ trianguloIsosceles(10);
 
 //Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
 
+function trianguloVazado (num) {
+    if (num%2 === 0) {
+        num -= 1;
+    }
+    for (let i = 1; i <= num; i+=1) {
+        if (i%2 === 1) {
+            if (i < 2) {
+            console.log(' '.repeat((num-i)/2) + '*'.repeat(i));
+            } else if (i < num) {
+                console.log(' '.repeat((num-i)/2) + '*' + ' '.repeat((i-2)) + '*');
+            } else if (i === num) {
+                console.log('*'.repeat(i));
+            }
+        }
+    }
+}
+
+trianguloVazado(10);
+
+// Faça um programa que diz se um número definido numa variável é primo ou não.
+// Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
+// Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+
+function ehPrimo (numero) { 
+
+    let divisoresCounter = 0;
+    
+    for (let i = 1; i <= numero; i+=1 ) {
+        if (numero%i === 0) {
+            divisoresCounter +=1;
+        }
+    }
+    
+    if (divisoresCounter > 2) {
+        console.log('não é primo');
+    } else {
+        console.log('é primo');
+    }
+}
+
+ehPrimo(7);
+ehPrimo(17);
+ehPrimo(20);
+ehPrimo(23);
