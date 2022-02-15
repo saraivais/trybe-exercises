1056dd8ffacba6b76c01655295f8ddca8f3a8bc0
saraivais <isadorasaraiva95@gmail.com>
2022-02-14 21:53:18 -0300

    5.3

--- 1.fundamentos/5.javascript-dom-eventos-e-web-storage/5.3.javascript-eventos/script.js
+++ 1.fundamentos/5.javascript-dom-eventos-e-web-storage/5.3.javascript-eventos/script.js
@@ -164,3 +164,4 @@ addEventListenerToClass();
 // Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
 // Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
 
+// try
