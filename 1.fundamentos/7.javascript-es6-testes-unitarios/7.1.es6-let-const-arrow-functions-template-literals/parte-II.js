e0ea94e0e7d026f2b5db9afe1545dfd097881db8
saraivais <isadorasaraiva95@gmail.com>
2022-03-05 20:43:40 -0300

    organizing

--- 1.fundamentos/7.javascript-es6-testes-unitarios/7.1.es6-let-const-arrow-functions-template-literals/parte-II.js
+++ 1.fundamentos/7.javascript-es6-testes-unitarios/7.1.es6-let-const-arrow-functions-template-literals/parte-II.js
@@ -26,10 +26,7 @@ function returnBiggerWord(string) {
 const biggerWord = (string) => string.split(' ').sort()[0];
 
 /*
-Crie uma página que contenha:
-Um botão ao qual será associado um event listener ;
-Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
-Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
+
 Crie um código JavaScript com a seguinte especificação:
 Não se esqueça de usar template literals
 Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
@@ -37,6 +34,7 @@ Exemplo:
 String determinada: "Tryber x aqui!"
 Parâmetro: "Bebeto"
 Retorno: "Tryber Bebeto aqui!"
+
 Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
 Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
 Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
