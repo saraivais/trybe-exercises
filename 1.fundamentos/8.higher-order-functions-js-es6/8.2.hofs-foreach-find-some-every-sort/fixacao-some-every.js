5cfa3137da5a3809f3063b4d6d6b8d9c815137c6
saraivais <isadorasaraiva95@gmail.com>
2022-03-10 19:00:59 -0300

    Exercicio/8.2

---
+++ 1.fundamentos/8.higher-order-functions-js-es6/8.2.hofs-foreach-find-some-every-sort/fixacao-some-every.js
@@ -0,0 +1,27 @@
+// Exercício 1: usar some()
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some((element) => element === name);
+}
+
+console.log(hasName(names, 'Ana'));
+
+//////////////////////////////////////////////////////////////////////////
+
+// Exercício 2: usar every()
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((element) => element.age >= minimumAge);
+}
+
+console.log(verifyAges(people, 18));
