60da940e42755b1d2b8807b7ab0aed8c8ae21d52
saraivais <isadorasaraiva95@gmail.com>
2022-03-20 14:34:10 -0300

    Organizing

---
+++ 1.fundamentos/8.higher-order-functions-js-es6/8.4.hofs-reduce/revisao-fixacao-2.js
@@ -0,0 +1,92 @@
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+// Agora crie uma função usando os dados dos estudantes que usamos no conteúdo do dia anterior, para mostrar na tela um relatório que diz em qual matéria o estudante foi melhor.
+
+// {nomeCompleto: 'nome sobrenome', melhorMateria: 'quimica'};
+
+const funcaoMelhorMateria = (arr) => {
+  const novoArray = arr.map((elemento) => {
+    const arrayMaterias = elemento.materias;
+    const aluno = {nomeCompleto: `${elemento.nome} ${elemento.sobrenome}`, melhorMateria: arrayMaterias.reduce((acc, aula) => acc.nota > aula.nota ? acc : aula).name}; //descobrir qual é a melhor matéria ~ maior valor - nota;
+    return aluno;
+  });
+  return novoArray;
+}
+
+console.log(funcaoMelhorMateria(estudantes));
+// recebe um array de objetos ~ devolve um array de objetos ~ map também retorna um array!
+
+// callback do map ~ (elemento, indice, array);
