002d2f8efa228a6d310cf037bb0f3f2a32f105f3
saraivais <isadorasaraiva95@gmail.com>
2022-03-22 10:52:34 -0300

    Exercicios de fixaçao 9.1

---
+++ 1.fundamentos/9.javascript-testes-assincronos/9.1.javascript-assincrono-callbacks/fixacao-callbacks.js
@@ -0,0 +1,57 @@
+// Definição da função userFullName
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+
+// Definição da função getUser
+const getUser = (param) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  // Retornamos nosso parâmetro, que será uma função (callback)
+  return param(userToReturn);
+};
+
+// Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
+getUser(userFullName);
+
+// ----------------------------------------------------------------- Exercício I
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn);
+  // Insira o retorno da função `getUser`
+};
+
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+
+// -------------------------------------------------------------------- Exercício II
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    // Retorne a `callback` passada como parâmetro na função `getUser`
+    // Dica: você pode manter o `console.log()`
+    console.log(callback(user));
+  }, delay());
+};
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
