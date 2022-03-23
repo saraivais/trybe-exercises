088fe9745cc2b9f5ee0e92ab85751cb613839e69
saraivais <isadorasaraiva95@gmail.com>
2022-03-23 15:05:12 -0300

    Exericios 9.2

--- 1.fundamentos/9.javascript-testes-assincronos/9.2.javascript-assincrono-fetch-API-async-await/bonus.js
+++ 1.fundamentos/9.javascript-testes-assincronos/9.2.javascript-assincrono-fetch-API-async-await/bonus.js
@@ -10,7 +10,7 @@ const fetchCoinTranslate = async () => {
 }
 
 const createLi = async (object) => {
-  const coinUl = document.querySelector('#coin-list');
+  const coinUl = document.querySelector('#coins-list');
   const newLi = document.createElement('li');
   const real = await fetchCoinTranslate();
   const realValue = parseInt(object.value, 10) * parseInt(real, 10);
@@ -43,3 +43,50 @@ fetchCoins();
 // const url = `${URLObj.baseUrl}${URLObj.endpoint}`;
 
 // refazer na mentoria~
+
+// ----------------------------------------------------------------------------------------- GABARITO
+// api.js
+// const fetchCoins = async () => {
+//   const url = 'https://api.coincap.io/v2/assets';
+
+//   const coins = await fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => data.data)
+//     .catch((error) => error.toString());
+
+//   return coins;
+// };
+
+// const fetchUsdCurrencies = async () => {
+//   const baseUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest'
+//   const usdEndpoint = '/currencies/usd.min.json'
+//   const url = baseUrl.concat(usdEndpoint);
+
+//   const usdCurrencies = await fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => data.usd)
+//     .catch((error) => error.toString());
+
+//   return usdCurrencies;
+// };
+
+// const setCoins = async () => {
+//   const coins = await fetchCoins();
+//   const usdCurrencies = await fetchUsdCurrencies();
+
+//   const coinsList = document.getElementById('coins-list');
+
+//   coins
+//     .filter((coin) => Number(coin.rank) <= 10)
+//     .forEach((coin) => {
+//       const newLi = document.createElement('li');
+//       const usdPrice = Number(coin.priceUsd);
+//       const brlPrice = usdPrice * usdCurrencies.brl;
+
+//       newLi.innerText = `${coin.name} (${coin.symbol}): ${brlPrice.toFixed(2)}`;
+
+//       coinsList.appendChild(newLi);
+//     });
+// };
+
+// window.onload = () => setCoins();
