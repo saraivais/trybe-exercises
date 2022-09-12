26c4b99e1b7cf4826c28a6885f1380c4c8936bc0
saraivais <isadorasaraiva95@gmail.com>
2022-09-12 11:33:40 -0300

    Exercicios 26.1

---
+++ 3.back-end/26.typescript/26.1.introducao-typescript/my-ts-scripts/dist/exercicio2.js
@@ -0,0 +1,17 @@
+"use strict";
+var mesesDoAno;
+(function (mesesDoAno) {
+    mesesDoAno["janeiro"] = "janeiro";
+    mesesDoAno["fevereiro"] = "fevereiro";
+    mesesDoAno["marco"] = "marco";
+    mesesDoAno["abril"] = "abril";
+    mesesDoAno["maio"] = "maio";
+    mesesDoAno["junho"] = "junho";
+    mesesDoAno["julho"] = "julho";
+    mesesDoAno["agosto"] = "agosto";
+    mesesDoAno["setembro"] = "setembro";
+    mesesDoAno["outubro"] = "outubro";
+    mesesDoAno["novembro"] = "novembro";
+    mesesDoAno["dezembro"] = "dezembro";
+})(mesesDoAno || (mesesDoAno = {}));
+console.log(mesesDoAno);
---
+++ 3.back-end/26.typescript/26.1.introducao-typescript/my-ts-scripts/exercicio2.ts
@@ -0,0 +1,16 @@
+enum mesesDoAno {
+  'janeiro' = 'janeiro',
+  'fevereiro' = 'fevereiro',
+  'marco' = 'marco',
+  'abril' = 'abril',
+  'maio' = 'maio',
+  'junho' = 'junho',
+  'julho' = 'julho',
+  'agosto' = 'agosto',
+  'setembro' = 'setembro',
+  'outubro' = 'outubro',
+  'novembro' = 'novembro',
+  'dezembro' = 'dezembro'
+}
+
+console.log(mesesDoAno);
